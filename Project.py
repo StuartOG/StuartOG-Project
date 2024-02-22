80c63459cc50233a794693063a4c4e0a386b845a
StuartOG <otisstu1@gmail.com>
2024-02-22 15:07:46 +0000

    waypoints
    create waypoints for the enemies to move towards

--- Project.py
+++ Project.py
@@ -14,18 +14,21 @@ screen = pygame.display.set_mode(size)
  
 pygame.display.set_caption("Otis' Computer Science Project")
 
+#importing the enemy image
 enemy_image = pygame.image.load('enemy_1.png').convert_alpha()
 
-
+#Creating Enemy Class
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, image, position):
+    def __init__(self, image, waypoints):
         super().__init__()
 
         self.image = image
+        self.waypoints = waypoints
+        self.position = self.waypoints[0]
         self.rect = self.image.get_rect()
-        self.rect.center = position
+        self.rect.center = self.position
 
-
+    #adding movement to the enemy
     def update(self):
         self.move()
     
@@ -33,16 +36,28 @@ class Enemy(pygame.sprite.Sprite):
         self.rect.x += 1
 
 
-enemy = Enemy(enemy_image, (350, 250))
+waypoints = [
+    (000, 300),
+    (150, 300),
+    (150, 100),
+    (550, 100),
+    (550, 300),
+    (700, 300)
+
+]
+
+enemy = Enemy(enemy_image, waypoints)
 
 all_sprites = pygame.sprite.Group()
 
 enemy_group = pygame.sprite.Group()
 
-for _ in range(1):
-    enemy = Enemy(enemy_image, (350, 250))
-    enemy_group.add(enemy)
-    all_sprites.add(enemy)
+
+
+
+enemy = Enemy(enemy_image, waypoints)
+enemy_group.add(enemy)
+all_sprites.add(enemy)
  
 # Loop until the user clicks the close button.
 done = False
@@ -60,7 +75,7 @@ while not done:
     # --- Game logic should go here
     
     enemy_group.update()
-    
+
     # --- Screen-clearing code goes here
  
     # Here, we clear the screen to white. Don't put other drawing commands
@@ -71,8 +86,10 @@ while not done:
     screen.fill(WHITE)
  
     # --- Drawing code should go here
- 
+    
+    pygame.draw.lines(screen, BLACK, False, waypoints)
     enemy_group.draw(screen)
+
 
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
