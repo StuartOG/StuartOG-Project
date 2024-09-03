f0b865bac5d3d418f1aa07afdcddf95a181fbf17
StuartOG <otisstu1@gmail.com>
2024-09-03 09:13:38 +0100

    adding buttons and range to towers

--- Project.py
+++ Project.py
@@ -7,11 +7,12 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+GREY = (128, 128, 128)
  
 pygame.init()
  
 # Set the width and height of the screen [width, height]
-size = (700, 500)
+size = (900, 500)
 screen = pygame.display.set_mode(size)
  
 pygame.display.set_caption("Otis' Computer Science Project")
@@ -55,6 +56,8 @@ class Enemy(pygame.sprite.Sprite):
 class Tower(pygame.sprite.Sprite):
     def __init__(self, s_width, s_length, position) -> None:
         super().__init__()
+        self.range = 90
+
         self.width = s_width
         self.length = s_length
         self.image = pygame.Surface([self.width, self.length])
@@ -62,6 +65,35 @@ class Tower(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = position
 
+        #creating transparent circle to show range
+        self.range_image = pygame.Surface((self.range * 2, self.range * 2))
+        self.range_image.fill((0, 0, 0))
+        self.range_image.set_colorkey((0, 0, 0))
+        pygame.draw.circle(self.range_image, GREY, (self.range, self.range), self.range)
+        self.range_image.set_alpha(100)
+        self.range_rect = self.range_image.get_rect()
+        self.range_rect.center = self.rect.center
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+#creating buttons
+class Buttons(pygame.sprite.Sprite):
+    def __init__(self, x_pos, y_pos):
+        super().__init__()
+        self.image = pygame.Surface([125, 50])
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x_pos, y_pos)
+    
+    def draw(self, surface):
+        action = False
+        pos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1:
+                action = True
+        return action
 waypoints = [
     (000, 300),
     (150, 300),
@@ -79,9 +111,13 @@ enemy_group = pygame.sprite.Group()
 tower_group = pygame.sprite.Group()
 
 enemy = Enemy(enemy_image, waypoints)
+
 enemy_group.add(enemy)
 all_sprites.add(enemy)
 
+tower_button = Buttons(750, 50)
+
+all_sprites.add(tower_button)
 # Loop until the user clicks the close button.
 done = False
  
@@ -97,13 +133,13 @@ while not done:
         #mouse click
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             mouse_pos = pygame.mouse.get_pos()
-            tower = Tower(40, 30, mouse_pos)
+            tower = Tower(30, 30, mouse_pos)
             tower_group.add(tower)
     
     # --- Game logic should go here
     
-    enemy_group.update()
-
+    all_sprites.update()
+    
     # --- Screen-clearing code goes here
  
     # Here, we clear the screen to white. Don't put other drawing commands
@@ -118,6 +154,10 @@ while not done:
     pygame.draw.lines(screen, BLACK, False, waypoints)
     enemy_group.draw(screen)
     tower_group.draw(screen)
+    for tower in tower_group:
+        Tower.draw(screen)
+    tower_button.draw(screen)
+
 
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
